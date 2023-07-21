dd3a2a8c733fddeeeb87e7cee0615cf9f74d5ca8
Joseph Adamo <140087801+joeadamo67@users.noreply.github.com>
2023-07-21 14:30:44 -0500

    Completed Exercise 1 and 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, num) {
+    let outputString = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i<num; i++){
+        outputString+=inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
