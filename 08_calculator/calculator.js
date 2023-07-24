92974eb82ae08eff2e4decff29409412a5554aa9
Joseph Adamo <140087801+joeadamo67@users.noreply.github.com>
2023-07-24 12:29:21 -0500

    Finished 7 and 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNums) {
+	let total = 0;
+  arrayOfNums.map(x => total+=x);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNums) {
+  let total = 1;
+  arrayOfNums.map(x => total*=x);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1;
+  }
+
+
+  let total = num;
+  for (let i = num-1; i > 0; i--){
+
+
+    total *= i;
+  }
+
+
+	return total;
 };
 
 // Do not edit below this line
