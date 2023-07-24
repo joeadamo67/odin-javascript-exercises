1af4a53e87911c7229924e39c04d941ff152ea11
Joseph Adamo <140087801+joeadamo67@users.noreply.github.com>
2023-07-24 13:18:20 -0500

    Completed 9 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    str = str.replace(/\s+/g, '');
+    str = str.toLowerCase();
+
+    let reverseStr = str.split("").reverse().join("");
+
+    if (str === reverseStr){
+        return true;
+    }
+    else {
+        return false;
+    }
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(numPos) {
+    if (numPos < 0){
+        return "OOPS";
+    }
+    let firstNum = 0;
+    let secondNum = 1;
+    for (let i = 1; i<numPos; i++){
+        
+        let current = firstNum+secondNum;
+        firstNum=secondNum;
+        secondNum=current;
+        
+    }
+    return secondNum;
 
 };
 
