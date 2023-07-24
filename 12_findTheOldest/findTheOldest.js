e67fa58b73ffca8f5a193b3bb784f6ed613dd70d
Joseph Adamo <140087801+joeadamo67@users.noreply.github.com>
2023-07-24 14:02:19 -0500

    Completed 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleList) {
 
+    let oldest;
+
+    peopleList.forEach(person => {
+        let age;
+        if (oldest === undefined){
+            oldest = person;
+        } else if (person.yearOfDeath == undefined){
+           age = ((new Date().getFullYear())-person.yearOfBirth);  
+        }  else {
+           age = person.yearOfDeath-person.yearOfBirth;
+        }
+
+
+        if (oldest.yearOfDeath == undefined){
+            if ((((new Date().getFullYear())-oldest.yearOfBirth) ) < age){
+                oldest = person;        
+            }
+         } else if ((oldest.yearOfDeath-oldest.yearOfBirth ) < age){
+            oldest = person;        
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
